f5cb7969ddadf36b4db47854e7d6c59fdbc13a67
Odaliz Rojas <odaliz.rojasroldan@aiesec.net>
2023-05-09 09:53:11 -0400

    Agregando ARN

--- modules/iam_role/main.tf
+++ modules/iam_role/main.tf
@@ -14,6 +14,9 @@ resource "aws_iam_role" "test_role" {
       },
     ]
   })
+  managed_policy_arns = concat([
+    aws_iam_policy.policy.arn
+  ],var.policy_arns)
 }
 
 resource "aws_iam_policy" "policy" {
